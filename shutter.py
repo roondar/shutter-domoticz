da3f923faad21fcbb989b1a0f5a825ec0edbcf1e
roondar <roondar56@gmail.com>
2019-12-15 12:45:46 +0100

    - add support to multiple shutters.

--- shutter.py
+++ shutter.py
@@ -12,8 +12,14 @@ logging.basicConfig(level=logging.INFO, format=config['logging']['format'])
 mqtt_server = config['mqtt']['host']
 mqtt_port = config['mqtt']['port']
 
-for s in config['shutters']:
-    shutter_name, shutter_idx = next(iter(s.items()))
+shutters = []
+
+for shutter in config['shutters']:
+    shutter_name, shutter_idx = next(iter(shutter.items()))
+    shutter_topic = f'cmnd/{shutter_name}/shutterposition'
+    shutters.append({'idx': shutter_idx,
+                     'topic': shutter_topic,
+                     'name': shutter_name})
 
 SHUTTER_CLOSE_STATE = config['shutter_state']['SHUTTER_CLOSE_STATE']
 SHUTTER_OPEN_STATE = config['shutter_state']['SHUTTER_OPEN_STATE']
@@ -24,44 +30,57 @@ sensor_name = config['tasmota']['sensor_name']
 client = mqtt.Client()
 logger = logging.getLogger(__name__)
 client.enable_logger(logger)
-publish_topic = f'cmnd/{shutter_name}/shutterposition'
+
+
+def get_shutter(idx):
+    return [s for s in shutters if s["idx"] == idx]
+
+
+def get_shutter_by_name(message):
+    for s in shutters:
+        if s['name'] in message:
+            return s
 
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(f"+/{shutter_name}/RESULT")
+    for shutter in shutters:
+        client.subscribe(f"+/{shutter['name']}/RESULT")
     client.subscribe(config['topics']['domoticz_out'])
 
 
 def on_message(client, userdata, msg):
     data = json.loads(msg.payload.decode("utf8"))
-    if msg.topic == config['topics']['domoticz_out'] and data['idx'] == shutter_idx:
+    if msg.topic == config['topics']['domoticz_out'] and get_shutter(data['idx']):
         nvalue = data['nvalue']
         svalue = data['svalue1']
+        shutter = get_shutter(data['idx'])[0]
         if nvalue == SHUTTER_OPEN_STATE:
-            logger.info(f"We opening shutter {shutter_name}")
-            client.publish(publish_topic, '100')
+            logger.info(f"We opening shutter {shutter['name']}")
+            client.publish(shutter["topic"], '100')
         elif nvalue == SHUTTER_CLOSE_STATE:
-            logger.info(f"We closing shutter {shutter_name}")
-            client.publish(publish_topic, '0')
+            logger.info(f"We closing shutter {shutter['name']}")
+            client.publish(shutter["topic"], '0')
         elif nvalue == SHUTTER_CUSTOM_STATE:
-            logger.info(f"We moving shutter {shutter_name} to position {svalue}")
-            client.publish(publish_topic, svalue)
+            logger.info(f"We moving shutter {shutter['name']} to position {svalue}")
+            client.publish(shutter["topic"], svalue)
 
-    elif shutter_name in msg.topic and sensor_name in data:
+    elif get_shutter_by_name(msg.topic) and sensor_name in data:
+        shutter = get_shutter_by_name(msg.topic)
         position = str(data[sensor_name]['Position'])
         logger.info(f"My current position is {position}")
         if position == "100":
-            payload = {"idx": shutter_idx, "nvalue": SHUTTER_OPEN_STATE, "svalue": position}
+            payload = {"idx": shutter['idx'], "nvalue": SHUTTER_OPEN_STATE, "svalue": position}
         elif position == "0":
-            payload = {"idx": shutter_idx, "nvalue": SHUTTER_CLOSE_STATE, "svalue": position}
+            payload = {"idx": shutter['idx'], "nvalue": SHUTTER_CLOSE_STATE, "svalue": position}
         else:
-            payload = {"idx": shutter_idx, "nvalue": SHUTTER_CUSTOM_STATE, "svalue": position}
+            payload = {"idx": shutter['idx'], "nvalue": SHUTTER_CUSTOM_STATE, "svalue": position}
         client.publish(config['topics']['domoticz_in'], json.dumps(payload))
 
 
 client.on_connect = on_connect
 client.on_message = on_message
 
+logger.info('Starting Server...')
 client.connect(mqtt_server, mqtt_port, 60)
 
 client.loop_forever()
